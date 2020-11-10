403cd52cfbb5f31c771f0ca96fab34fa3f74a592
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-11 00:10:27 +0300

    fixed issues

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import promptly from 'promptly';
 
-async function askName () {
-    const name = await promptly.prompt('May i have your name?: ');
-    console.log(`Hello, ${name}!`);
-  };
+async function askName() {
+  const name = await promptly.prompt('May i have your name?: ');
+  console.log(`Hello, ${name}!`);
+}
+
 export default askName;
