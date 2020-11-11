5630e04a281a30dd32305038f3d4c64329132100
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-11 22:59:51 +0300

    file edited

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-import askName from '../src/cli.js';
+import promptly from 'promptly';
 
 console.log('Welcome to the Brain Games!');
 
-askName();
+async function askName() {
+  const name = await promptly.prompt('May i have your name?: ');
+  console.log(`Hello, ${name}!`);
+}
