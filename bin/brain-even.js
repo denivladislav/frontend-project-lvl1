9b4320298fe0e5945bd099265d45429697f74846
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-11 23:03:09 +0300

    mistakes fixed

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import promptly from 'promptly';
 
-console.log('Welcome to the Brain Games!');
-
 async function askName() {
   const name = await promptly.prompt('May i have your name?: ');
   console.log(`Hello, ${name}!`);
 }
+
+console.log('Welcome to the Brain Games!');
+
+askName();
