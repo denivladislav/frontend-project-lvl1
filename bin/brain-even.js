b54974961b025512e871fb818f9ddfeb2babeb95
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-11 23:14:05 +0300

    greeting moved into askName function

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,10 +2,9 @@
 import promptly from 'promptly';
 
 async function askName() {
+  console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May i have your name?: ');
   console.log(`Hello, ${name}!`);
 }
-
-console.log('Welcome to the Brain Games!');
 
 askName();
