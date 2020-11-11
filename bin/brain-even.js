05407aad7c3655548e82414644729e281e95077a
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-12 01:03:44 +0300

    brainEvenStart added

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-import promptly from 'promptly';
+import brainEvenStart from '../src/index.js';
 
-async function askName() {
-  console.log('Welcome to the Brain Games!');
-  const name = await promptly.prompt('May i have your name?: ');
-  console.log(`Hello, ${name}!`);
-}
-
-askName();
+brainEvenStart();
