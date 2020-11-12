91deb32ad04e7dd59826683cc6c6b3f9e1fb6a82
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-12 14:08:44 +0300

    edited due to creation of brain-even logic file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
-import promptly from 'promptly';
-
 function getRandomInt(min, max) {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
 }
 
-async function brainEvenStart() {
-  console.log('Welcome to the Brain Games!');
-  const name = await promptly.prompt('May i have your name?: ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let correctAnswerCounter = 0;
-
-  while (correctAnswerCounter < 3) {
-    const number = getRandomInt(-1000, 1001);
-    const answer = await promptly.prompt(`Question: ${number}`);
-    const isEven = (number % 2 === 0);
-    if ((answer === 'yes' && isEven) || (answer === 'no' && !isEven)) {
-      correctAnswerCounter += 1;
-      console.log('Correct!');
-    } else {
-      if (isEven) {
-        console.log('Wrong answer ;(. The correct answer was \'yes\'.');
-      } else console.log('Wrong answer ;(. The correct answer was \'no\'.');
-      console.log(`Let's try again, ${name}!`);
-      return null;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-  return null;
-}
-
-export default brainEvenStart;
+export default getRandomInt;
