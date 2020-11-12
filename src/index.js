b1a2e60478e0f0438a278189113e662cdfeafd30
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-12 17:51:16 +0300

    getRandomMathSign function added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+const correctAnswerNumber = 3;
+
 function getRandomInt(min, max) {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
 }
 
-function getRandomMathSign() {
-  const mathSigns = ['+', '-', '*'];
-  const signIndex = getRandomInt(0, 4);
+function getRandomMathSign(mathSigns = ['+', '-', '*']) {
+  const signIndex = getRandomInt(0, mathSigns.length);
   return mathSigns[signIndex];
 }
 
-export { getRandomInt, getRandomMathSign };
+export { getRandomInt, getRandomMathSign, correctAnswerNumber };
