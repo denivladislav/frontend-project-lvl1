3c1922106012dd158e4456e60d04ee8ca48e9f28
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-12 17:51:59 +0300

    calculation logic edited

---
+++ src/games/brain-calcLogic.js
@@ -0,0 +1,47 @@
+import promptly from 'promptly';
+import { getRandomInt, getRandomMathSign, correctAnswerNumber } from '../index.js';
+
+async function brainCalcStart() {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May i have your name?: ');
+  console.log(`Hello, ${name}!\nWhat is the result of the expression?.`);
+  let correctAnswerCounter = 0;
+
+  while (correctAnswerCounter < correctAnswerNumber) {
+    const number1 = getRandomInt(-100, 100);
+    const number2 = getRandomInt(0, 100);
+    const mathSign = getRandomMathSign();
+    const answer = await promptly.prompt(`Question: ${number1} ${mathSign} ${number2} =`);
+    const integerAnswer = Number(answer);
+
+    console.log(`Your answer: ${integerAnswer}`);
+
+    let correctAnswer = 0;
+    switch (mathSign) {
+      case '+':
+        correctAnswer = number1 + number2;
+        break;
+      case '-':
+        correctAnswer = number1 - number2;
+        break;
+      case '*':
+        correctAnswer = number1 * number2;
+        break;
+      default:
+        console.log('Unknown operation.');
+        return null;
+    }
+    if (correctAnswer === integerAnswer) {
+      correctAnswerCounter += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`Wrong answer ;(. The correct answer was '${correctAnswer}'\nLet's try again, ${name}!.`);
+      return null;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+  return null;
+}
+
+export default brainCalcStart;
