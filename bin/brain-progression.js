1d429f8264baab0bc15ca332026f0bc14937786c
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-13 20:48:27 +0300

    progression functions added

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import brainProgressionStart from '../src/games/brain-progressionLogic.js';
 import { showGreetingMessage } from '../src/index.js';
 
 showGreetingMessage();
+
+brainProgressionStart();
---
+++ src/games/brain-progressionLogic.js
@@ -0,0 +1,36 @@
+import promptly from 'promptly';
+import {
+  getMathProgression, hideProgressionElement,
+  getRandomInt, correctAnswerAmount, showLoseMessage,
+  showWinMessage, showCorrectAnswerMessage,
+} from '../index.js';
+
+async function brainProgressionStart() {
+  const name = await promptly.prompt('May i have your name?: ');
+  console.log(`Hello, ${name}!\nWhat number is missing in the progression?`);
+  let correctAnswerCounter = 0;
+
+  while (correctAnswerCounter < correctAnswerAmount) {
+    const progression = getMathProgression();
+    const indexOfHiddenElement = getRandomInt(0, progression.length);
+    const hiddenElement = progression[indexOfHiddenElement];
+    const integerHiddenElement = Number(hiddenElement.trim());
+    const progressionWithHidden = hideProgressionElement(progression, indexOfHiddenElement);
+    const answer = await promptly.prompt(`Question: ${progressionWithHidden.join('')}`);
+    const integerAnswer = Number(answer);
+
+    console.log(`Your answer: ${integerAnswer}`);
+
+    if (integerAnswer === integerHiddenElement) {
+      correctAnswerCounter += 1;
+      showCorrectAnswerMessage();
+    } else {
+      showLoseMessage(answer, integerHiddenElement, name);
+      return null;
+    }
+  }
+  showWinMessage(name);
+  return null;
+}
+
+export default brainProgressionStart;
