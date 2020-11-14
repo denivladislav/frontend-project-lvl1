ca24174f25406d6dbb737040b7910e65356d45fc
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-14 14:18:49 +0300

    function brainPrimeStart imported

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 import { showGreetingMessage } from '../src/index.js';
+import brainPrimeStart from '../src/games/brain-primeLogic.js';
 
 showGreetingMessage();
+
+brainPrimeStart();
