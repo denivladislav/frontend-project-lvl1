b2b1b7e9d8e85304dbb59c2942d5355ad1a62cd3
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-20 23:36:27 +0300

    brain-calcLogic edited, some functions transfered from index.js

--- src/games/brain-calcLogic.js
+++ src/games/brain-calcLogic.js
@@ -1,37 +1,29 @@
-import promptly from 'promptly';
-import {
-  getRandomInt, getRandomMathSign, makeNumericOperation,
-  correctAnswerAmount, showLoseMessage,
-  showWinMessage, showCorrectAnswerMessage,
-} from '../index.js';
+const numbersCalcRange = [0, 100];
 
-async function brainCalcStart() {
-  const name = await promptly.prompt('May i have your name?');
-  console.log(`Hello, ${name}!\nWhat is the result of the expression?`);
-  let correctAnswerCounter = 0;
+const numbersCalcAmount = 2;
 
-  while (correctAnswerCounter < correctAnswerAmount) {
-    const number1 = getRandomInt(-100, 100);
-    const number2 = getRandomInt(0, 100);
-    const mathSign = getRandomMathSign();
-    const answer = await promptly.prompt(`Question: ${number1} ${mathSign} ${number2} =`);
-    const integerAnswer = Number(answer);
+const mathSignsCalcAmount = 1;
 
-    console.log(`Your answer: ${answer}`);
-
-    const correctAnswer = makeNumericOperation(number1, mathSign, number2);
-
-    if (correctAnswer === integerAnswer) {
-      correctAnswerCounter += 1;
-      showCorrectAnswerMessage();
-    } else {
-      showLoseMessage(answer, correctAnswer, name);
-      return null;
-    }
-  }
-
-  showWinMessage(name);
-  return null;
+function makeCalcQuestion(numbers, mathSigns) {
+  return `${numbers[0]} ${mathSigns[0]} ${numbers[1]}`;
 }
 
-export default brainCalcStart;
+function makeCalcAnswer(numbers, mathSigns) {
+  switch (mathSigns[0]) {
+    case '+':
+      return String(numbers[0] + numbers[1]);
+    case '-':
+      return String(numbers[0] - numbers[1]);
+    case '*':
+      return String(numbers[0] * numbers[1]);
+    default:
+      console.log('Unknown operation.');
+      return null;
+  }
+}
+
+export {
+  makeCalcQuestion, makeCalcAnswer,
+  numbersCalcRange, numbersCalcAmount,
+  mathSignsCalcAmount,
+};
