e416e16f99e1d54117ff6224285fcfd7998fb205
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-29 16:31:04 +0300

    function renamed

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
-import {
-  questionEvenText,
-  makeEvenQuestion, makeEvenAnswer,
-  numbersEvenRange, numbersEvenAmount,
-} from '../src/games/brain-evenLogic.js';
+import { questionText, makeGameData } from '../src/games/brain-evenLogic.js';
 
-import { gameStart } from '../src/index.js';
+import gameStart from '../src/index.js';
 
-gameStart(questionEvenText, makeEvenQuestion, makeEvenAnswer, numbersEvenAmount, numbersEvenRange);
+gameStart(questionText, makeGameData);
