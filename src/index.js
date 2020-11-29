8f599c1b5da93daeac2486e87d7b01d91e8fbeaa
vvvhatislove <vladenisov1234@yandex.ru>
2020-11-29 16:30:28 +0300

    function gameStart reorganised; several functions moved to other modules

--- src/index.js
+++ src/index.js
@@ -2,49 +2,7 @@ import promptly from 'promptly';
 
 const correctAnswerAmount = 3;
 
-function getRandomInt(min, max) {
-  const minNum = Math.ceil(min);
-  const maxNum = Math.floor(max);
-  return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
-}
-
-function getRandomMathSign(mathSigns = ['+', '-', '*']) {
-  const signIndex = getRandomInt(0, mathSigns.length);
-  return mathSigns[signIndex];
-}
-
-function getGcd(number1, number2) {
-  let a = number1;
-  let b = number2;
-  while (a !== 0 && b !== 0) {
-    if (a > b) {
-      a %= b;
-    } else b %= a;
-  }
-  return a + b;
-}
-
-function isNumberPrime(number) {
-  if (number === 1) {
-    return `${number}: is a special case`;
-  }
-  const divisionTo1 = (x, y) => {
-    if (y === 1) {
-      return true;
-    }
-    if (x % y === 0) {
-      return false;
-    } return divisionTo1(x, y - 1);
-  };
-
-  if (divisionTo1(number, number - 1)) {
-    return true;
-  }
-  return false;
-}
-
-async function gameStart(questionText = 'Question Text needed', makeQuestion = undefined, makeAnswer = undefined, numbersAmount = 10,
-  numbersRange = [-100, 100], mathSignsAmount = 0) {
+async function gameStart(questionText, makeGameData) {
   console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May i have your name?');
   console.log(`Hello, ${name}!\n${questionText}`);
@@ -52,18 +10,9 @@ async function gameStart(questionText = 'Question Text needed', makeQuestion = u
   let correctAnswerCounter = 0;
 
   while (correctAnswerCounter < correctAnswerAmount) {
-    const numbers = [];
-    const mathSigns = [];
-    for (let i = 0; i < numbersAmount; i += 1) {
-      numbers.push(getRandomInt(numbersRange[0], numbersRange[1]));
-    }
-    if (mathSignsAmount !== 0) {
-      for (let i = 0; i < mathSignsAmount; i += 1) {
-        mathSigns.push(getRandomMathSign());
-      }
-    }
-    const question = makeQuestion(numbers, mathSigns);
-    const correctAnswer = String(makeAnswer(numbers, mathSigns));
+    const gameData = makeGameData();
+    const question = gameData[0];
+    const correctAnswer = gameData[1];
     const answer = await promptly.prompt(`Question: ${question}`);
 
     console.log(`Your answer: ${answer}`);
@@ -80,7 +29,4 @@ async function gameStart(questionText = 'Question Text needed', makeQuestion = u
   return null;
 }
 
-export {
-  getRandomInt, getRandomMathSign,
-  getGcd, isNumberPrime, gameStart,
-};
+export default gameStart;
