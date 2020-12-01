5c3bc486280c9ebbd19e02fcdafe857f64104012
vvvhatislove <vladenisov1234@yandex.ru>
2020-12-01 23:12:03 +0300

    isPrime const fixed; getRandomInt range fixed

--- src/games/brain-primeLogic.js
+++ src/games/brain-primeLogic.js
@@ -4,23 +4,21 @@ import playGame from '../index.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number === 1) {
-    return `${number}: is a special case`;
-  }
   const divisionTo1 = (x, y) => {
     if (y === 1) {
       return true;
     }
     if (x % y === 0) {
       return false;
-    } return divisionTo1(x, y - 1);
+    }
+    return divisionTo1(x, y - 1);
   };
 
   return divisionTo1(number, number - 1);
 };
 
 const makeGameData = () => {
-  const number = getRandomInt(0, 1000);
+  const number = getRandomInt(1, 1000);
 
   const question = number;
   const answer = isPrime(number) ? 'yes' : 'no';
